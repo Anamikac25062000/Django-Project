75605a46403776882989b8e9a320d55c5a2bf6db
Anamikac25062000 <140783864+Anamikac25062000@users.noreply.github.com>
2023-10-05 15:03:03 +0530

    Delete template/template directory

--- template/template/design/app1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from app1.models import Place
-admin.site.register(Place)
-
-from app1.models import Team
-admin.site.register(Team)
-# Register your models here.
